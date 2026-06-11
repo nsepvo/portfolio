1876f7e6f7cfe8992b6b087b397e306d2c5b0143
nsepvo <156858481+nsepvo@users.noreply.github.com>
2026-06-11 12:58:43 +1000

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <div className="noise" />
+        {children}
+      </body>
+    </html>
+  );
+}
